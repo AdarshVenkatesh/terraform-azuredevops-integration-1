ab64d2618898669347275bc11c4483cca9597e41
Adarsh Venkatesh Bodineni <adarshvenkatesh169@gmail.com>
2022-02-15 12:09:31 -0800

    added underscore

--- terraform-manifests/aks-shared-resources/resource-groups.tf
+++ terraform-manifests/aks-shared-resources/resource-groups.tf
@@ -1,7 +1,7 @@
 ##############################################################################################################
 # RESOURCES (Resopurce Group)
 ##############################################################################################################
-resource "azurerm-resource-group" "k8s_rg" {
+resource "azurerm_resource_group" "k8s_rg" {
   name      = local.aks_rg_name
   location  = var.resourcegroup_location
   tags      = local.common_tags
